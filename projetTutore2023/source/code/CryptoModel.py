d6fe7d235655a2a09e9e729c57155efbeff72f6e
djibril ba <djibril99@github.com>
2023-07-13 15:51:36 +0000

    3eme commit

--- projetTutore2023/source/code/CryptoModel.py
+++ projetTutore2023/source/code/CryptoModel.py
@@ -1,10 +1,85 @@
 class CryptoModel:
-        
         def __init__(self) :
-                #declarrer les attributs de l'objet dont on aura besoin               
+                #declarrer les attributs de l'objet dont on aura besoin  
+                
+                self.price = 0
+                self.name = ''
+                self.symbol = ''
+                self.slug = ''
+                self.cmc_rank = 0
+                self.num_market_pairs = 0
+                
+                             
                 pass
         def load(self, data):
                 #charger les donnees dans l'objet
                 #data est un dictionnaire et les cles sont les noms des attributs
-                # une 
-                pass
+                # pour traduis ls donnees de cette objet en format json on utilise la fonction dumps de json sur l'objet en question
+                
+                #pour traduire les donne
+                for key, value in data.items():
+                        self.charge(key, value)
+        
+        #utiliser la recursivité pour charger les donnees de l'objet
+        def charge(self,key , value):
+                #verifier si c est un dict
+                if isinstance(value, dict):
+                        self.load(value)
+                else:
+                        #verifier si l'attribut existe
+                        if hasattr(self, key):
+                                setattr(self,key,value)
+        
+        
+        
+        
+        
+                """
+                 {
+            "id": 1,
+            "name": "Bitcoin",
+            "symbol": "BTC",
+            "slug": "bitcoin",
+            "cmc_rank": 5,
+            "num_market_pairs": 500,
+            "circulating_supply": 16950100,
+            "total_supply": 16950100,
+            "max_supply": 21000000,
+            "infinite_supply": false,
+            "last_updated": "2018-06-02T22:51:28.209Z",
+            "date_added": "2013-04-28T00:00:00.000Z",
+            "tags": [
+                "mineable"
+            ],
+            "platform": null,
+            "self_reported_circulating_supply": null,
+            "self_reported_market_cap": null,
+            "quote": {
+                "USD": {
+                    "price": 9283.92,
+                    "volume_24h": 7155680000,
+                    "volume_change_24h": -0.152774,
+                    "percent_change_1h": -0.152774,
+                    "percent_change_24h": 0.518894,
+                    "percent_change_7d": 0.986573,
+                    "market_cap": 852164659250.2758,
+                    "market_cap_dominance": 51,
+                    "fully_diluted_market_cap": 952835089431.14,
+                    "last_updated": "2018-08-09T22:53:32.000Z"
+                },
+                "BTC": {
+                    "price": 1,
+                    "volume_24h": 772012,
+                    "volume_change_24h": 0,
+                    "percent_change_1h": 0,
+                    "percent_change_24h": 0,
+                    "percent_change_7d": 0,
+                    "market_cap": 17024600,
+                    "market_cap_dominance": 12,
+                    "fully_diluted_market_cap": 952835089431.14,
+                    "last_updated": "2018-08-09T22:53:32.000Z"
+                }
+            }
+        },
+                """
+        
--- projetTutore2023/source/main.py
+++ projetTutore2023/source/main.py
@@ -1,19 +1,37 @@
 #code principale
 import streamlit as st
 import pandas as pd
+from json import dumps
 
 from source.code.DataReader import DataReader
+from source.code.CryptoModel import CryptoModel
 
+###########################################
+#recuperer les donnees depuis du lien
 
 URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 API_KEY = 'ded5b890-0e71-41e4-b097-a3e73ec43f99'
 data_reader = DataReader(URL, API_KEY)
 
-data = data_reader.get_data()
-if data is None:
+liste_data = data_reader.get_data()
+if len(liste_data) == 0:
         st.error('Error while loading data')
         st.stop()
 else:
-        print(data)
-        df = pd.DataFrame(data)
+        #convertir la liste en dictionnaire
+        data_json = {}
+        #recuperer les attritus de la classe CryptoModel
+        
+        listeKeys = [key for key in CryptoModel().__dict__]
+        for key in listeKeys:
+                data_json[key] = []
+        for crypto_data in liste_data:
+                #ajouter les donnees de chaque objet dans la liste
+                dict_crupto = crypto_data.__dict__
+                for key in listeKeys:
+                        data_json[key].append(dict_crupto[key])
+                        
+                
+                
+        df = pd.DataFrame(data_json)
         st.dataframe(df)
